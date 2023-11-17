7511144a0e45cf8465e7e7572f4afe7f3d4c7bbe
BagusFary <bagusfaryananta@gmail.com>
2023-11-17 14:09:16 +0700

    First Commit

---
+++ app/api/contact/route.js
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import Contact from "../../models/contact";
+import mongoose from "mongoose";
+import connectDB from "../../lib/mongodb"; 
+
+
+export async function POST(req) {
+    const { fullname, email, message} = await req.json();
+
+    try {
+        await connectDB();
+        await Contact.create({fullname, email, message})
+
+        return NextResponse.json({
+            msg: ["Message sent successfully"], success: true 
+        })
+    } catch(error) {
+        if (error instanceof mongoose.Error.ValidationError) {
+            let errorList = [];
+            for (let e in error.errors) {
+                errorList.push(e.message);
+            }
+
+            return NextResponse.json({msg: errorList})
+        } else {
+            return NextResponse.json({msg: "Unable to send message."});
+        }
+    }
+}
